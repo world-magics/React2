0b5506cbf939927a1526cc329a84f6a9bf6e6c45
xray-ray <sherzodyarashev1997@gmail.com>
2021-12-16 12:48:35 +0500

    Usememo

--- src/tutorial/12-memo-useMemo-useCallback/setup/index.js
+++ src/tutorial/12-memo-useMemo-useCallback/setup/index.js
@@ -7,9 +7,30 @@ const url = 'https://course-api.com/javascript-store-products'
 
 // every time props or state changes, component re-renders
 
+const calculateMostExpensive=(data)=>{
+   return data.reduce((total,item)=>{
+     const price=item.fields.price;
+     if(price>=total){
+       total=price;
+     }
+     return total;
+   },0)/100
+}
+
 const Index = () => {
   const { products } = useFetch(url)
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const [cart,setCart]=useState(0);
+
+  const addToCart=useCallback(()=>{
+    setCart(cart+1);
+  },[cart]);
+
+  const mostExpensive=useMemo(()=>calculateMostExpensive
+  (products),[
+    products,
+  ]);
 
   return (
     <>
@@ -17,22 +38,31 @@ const Index = () => {
       <button className='btn' onClick={() => setCount(count + 1)}>
         click me
       </button>
-      <BigList products={products} />
+      <h1 style={{marginTop:'3rem'}}>cart:{cart}</h1>
+      <h1>Most Expensive:${mostExpensive}</h1>
+      <BigList products={products} addToCart={addToCart} />
     </>
   )
 }
 
-const BigList = ({ products }) => {
+const BigList = React.memo(({ products,addToCart }) => {
+  
   return (
     <section className='products'>
       {products.map((product) => {
-        return <SingleProduct key={product.id} {...product}></SingleProduct>
+        return <SingleProduct key={product.id} {...product} 
+        addToCart={addToCart}>
+
+        </SingleProduct>
       })}
     </section>
-  )
-}
+  );
+});
 
-const SingleProduct = ({ fields }) => {
+const SingleProduct = ({ fields,addToCart}) => {
+  useEffect(()=>{
+    console.count('big list called');
+  })
   let { name, price } = fields
   price = price / 100
   const image = fields.image[0].url
@@ -42,7 +72,8 @@ const SingleProduct = ({ fields }) => {
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>${price}</p>
+      <button onClick={addToCart}>add to Card</button>
     </article>
   )
 }
-export default Index
+export default Index;
